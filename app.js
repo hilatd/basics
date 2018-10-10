8f0b00d75250dd68e9df78d096c86da191863bd2
hilatd <43959638+hilatd@users.noreply.github.com>
2018-10-10 10:20:10 +0300

    event emitter

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req,res){
-    res.send('hello world!');
+var events = require('events');
+
+var eventsEmitter = new events.EventEmitter();
+
+var connectHandler = function connected(){
+    console.log('connection sucesful');
+
+    eventsEmitter.emit('data_received');
+}
+
+eventsEmitter.on('connection',connectHandler);
+
+eventsEmitter.on('data_received',function(){
+    console.log('data received succesfully');
 });
 
+
+app.get('/', function (req,res){
+   
+    eventsEmitter.emit('connection');
+    res.send('finished');
+    
+});
+app.get('/t', function (req,res){
+    res.send('you are on a magical mystery tour!');
+    
+});
 app.listen(3000, function(){
     console.log('Example app listening on port 3000');
 });
